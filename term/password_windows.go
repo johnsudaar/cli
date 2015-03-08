2a923ee03346bf37e0bcac9c8af1e988998e7edd
Soulou <leo@unbekandt.eu>
2015-03-08 19:02:41 +0100

    indows, password: don't display password in clear

--- term/password_windows.go
+++ term/password_windows.go
@@ -7,6 +7,16 @@ import (
 func Password(prompt string) (string, error) {
 	fmt.Print(prompt)
 	var pass string
-	_, err := fmt.Scanln(&pass)
-	return pass, err
+	var c rune
+	for c != '\n' {
+		_, err := fmt.Scanf("%c", &c)
+		if err != nil {
+			return "", err
+		}
+		if c != '\n' {
+			pass = fmt.Sprintf("%s%c", pass, c)
+		}
+		fmt.Print("\b")
+	}
+	return pass, nil
 }
